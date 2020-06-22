1a16c1813b236247e6adf3a136bfc2d6fe62aae5
Bliss <pisit.w@23perspective.com>
2020-06-22 16:23:46 +0700

    initial

--- SafeAreaInset/ViewController.swift
+++ SafeAreaInset/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.additionalSafeAreaInsets.top = 100
         // Do any additional setup after loading the view.
+        colorView.setGradientBackground()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: nil, action: nil)
+        
+        title = "Title"
+        
     }
 
+    @IBAction func sliderSlided(_ sender: Any) {
+        let top: CGFloat = (CGFloat(200 * ((sender as? UISlider)?.value ?? 1) - 0.5))
+        navigationController?.additionalSafeAreaInsets.top = top
 
+    }
+
+    @IBAction func slider2Slided(_ sender: Any) {
+        let top: CGFloat = (CGFloat(100 * ((sender as? UISlider)?.value ?? 1)))
+        additionalSafeAreaInsets.top = top
+
+    }
 }
 
+extension UIView {
+    
+
+    func setGradientBackground() {
+        let colorTop =  UIColor(red: 0.26, green: 0.40, blue: 0.97, alpha: 1.00)
+.cgColor
+        let colorBottom = UIColor(red: 0.91, green: 0.58, blue: 0.67, alpha: 1.00)
+.cgColor
+
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.bounds
+
+        self.layer.insertSublayer(gradientLayer, at:0)
+    }
+}
