ff6264a4833e0aef0ba37170416f3f42e299d540
Bliss <pisit.w@23perspective.com>
2020-06-22 14:37:57 +0700

    Initial Commit

---
+++ SafeAreaInset/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SafeAreaInset
+//
+//  Created by Pisit W on 22/6/2563 BE.
+//  Copyright © 2563 blyscuit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SafeAreaInsetTests/SafeAreaInsetTests.swift
@@ -0,0 +1,34 @@
+//
+//  SafeAreaInsetTests.swift
+//  SafeAreaInsetTests
+//
+//  Created by Pisit W on 22/6/2563 BE.
+//  Copyright © 2563 blyscuit. All rights reserved.
+//
+
+import XCTest
+@testable import SafeAreaInset
+
+class SafeAreaInsetTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
